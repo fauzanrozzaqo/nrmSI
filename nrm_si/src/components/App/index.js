ef5eac54af8d8b09c36dae7d3430849471f56846
fauzanrozzaqo <frkusuma@gmail.com>
2019-12-26 13:52:33 +0700

    Add some page

---
+++ nrm_si/src/components/App/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+//import { add } from './math.js';
+import Intro from '../Intro';
+import 'whatwg-fetch';
+import Series from '../../containers/Series';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series List</h1>
+        </header>
+        <Intro message="Here you can find all of your most loved series" />
+        <Series />
+      </div>
+    );
+  }
+}
+
+export default App;
